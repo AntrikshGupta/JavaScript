95abcb52d3bae1e3d37e06657a3e3331d0e3f7b9
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-06-06 17:48:40 +0000

    dt

---
+++ Basics/datatypes.js
@@ -0,0 +1,7 @@
+let age = 25                        //number type
+let phoneNumber = 8456834764n       //bigint type - use n at last or use BigInt()
+let name = "James Bond"             //stringtype ""
+let isHuman = true                  //boolean values : true or false
+let wife = null                     //null is an object
+let kids                            //undefined is of type undefined
+
