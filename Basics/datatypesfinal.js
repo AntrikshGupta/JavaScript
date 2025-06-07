fd7cd06da45417ba23dbaee4ca5759ff5a75e565
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-06-07 18:02:19 +0000

    dt_final

---
+++ Basics/datatypesfinal.js
@@ -0,0 +1,31 @@
+// Primitive data types: Number, String, Symbol, Boolean, null, undefined, BigInt 
+const score = 100
+
+const name = "JavaScript"
+
+const isPass = true
+
+const isOut = null
+
+let email;
+
+const contact = 9874587833n
+
+const title = Symbol("Avenger")
+
+// Reference or Non-Primitive type: Array, Objects, Functions
+
+const heroes = ["IronMan","Captain","Thor","Hulk","Natasha","Hawkeye"]
+
+const avengers_assemble=function(){
+    console.log("AVENGERSSSSSSSS .... Assemble");
+    
+}
+
+const objectsss={
+    name: "Technology,Shield,Mjolnir,Smash,Shooters,Arrow",
+    total: 6
+}
+
+avengers_assemble()
+console.log(heroes);
