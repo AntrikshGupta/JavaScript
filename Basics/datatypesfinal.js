be63067cf09562045e8e6394626cf3d02e47e1ac
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-06-07 18:43:19 +0000

    fin

--- Basics/datatypesfinal.js
+++ Basics/datatypesfinal.js
@@ -1,31 +1,63 @@
 // Primitive data types: Number, String, Symbol, Boolean, null, undefined, BigInt 
-const score = 100
+// const score = 100
 
-const name = "JavaScript"
+// const name = "JavaScript"
 
-const isPass = true
+// const isPass = true
 
-const isOut = null
+// const isOut = null
 
-let email;
+// let email;
 
-const contact = 9874587833n
+// const contact = 9874587833n
 
-const title = Symbol("Avenger")
+// const title = Symbol("Avenger")
 
-// Reference or Non-Primitive type: Array, Objects, Functions
+// // Reference or Non-Primitive type: Array, Objects, Functions
 
-const heroes = ["IronMan","Captain","Thor","Hulk","Natasha","Hawkeye"]
+// const heroes = ["IronMan","Captain","Thor","Hulk","Natasha","Hawkeye"]
 
-const avengers_assemble=function(){
-    console.log("AVENGERSSSSSSSS .... Assemble");
+// const avengers_assemble=function(){
+//     console.log("AVENGERSSSSSSSS .... Assemble");
     
-}
+// }
 
-const objectsss={
-    name: "Technology,Shield,Mjolnir,Smash,Shooters,Arrow",
-    total: 6
-}
+// const objectsss={
+//     name: "Technology,Shield,Mjolnir,Smash,Shooters,Arrow",
+//     total: 6
+// }
 
-avengers_assemble()
-console.log(heroes);
+// avengers_assemble()
+// console.log(heroes);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack memory ->  primitive types, copy of variables is used, original remains intact
+
+// let nameYt = "hello"
+// let alias = nameYt
+// console.log(alias);
+
+// alias = "Bharat"
+// console.log(nameYt);
+// console.log(alias);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+userTwo.email = "user@amzn.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
+// Heap memory -> non-primitive types, reference of the variables is used
