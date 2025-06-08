71b2ee60b8ad94742d5407b2d061b37bd40848f7
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-06-08 07:22:00 +0000

    math

---
+++ Basics/numberAndMaths.js
@@ -0,0 +1,29 @@
+const num = 400
+// console.log(num);
+
+const bal = new Number(200)
+// console.log(bal);
+
+// console.log(bal.toString());
+// console.log(bal.toFixed(2))
+
+const otherNum = 123.8967
+// console.log(otherNum.toPrecision(5));
+
+// console.log(Math.abs(-46));
+// console.log(Math.round(456.3486));
+// console.log(Math.ceil(5.56));
+// console.log(Math.floor(89.3455));
+// console.log(Math.sqrt(24));
+// console.log(Math.pow(7,3));
+// console.log(Math.min(5,3,6,2,1,6,4));
+// console.log(Math.max(5,3,6,2,1,6,4));
+// console.log(Math.random());
+
+// to generate a random number in a given range 
+
+const min = 10
+const max = 20
+console.log(Math.round((Math.random() * (max - min + 1)) + min))
+
+
