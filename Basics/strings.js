4ecbf432f13ebb4712fd13ac06b0693e3f73929d
AntrikshGupta <48564308+AntrikshGupta@users.noreply.github.com>
2025-06-08 05:27:05 +0000

    str

--- Basics/strings.js
+++ Basics/strings.js
@@ -1,5 +1,22 @@
 const name = "XYZ"
 const repo = 20 
-console.log(name + " has " + repo + " repo on github.");
-console.log(`${name} has ${repo} repos on github`);
+// console.log(name + " has " + repo + " repo on github.");
+// console.log(`${name} has ${repo} repos on github`);
+
+const testStr = "  Antriksh-Gupta-Indore  "
+
+console.log(testStr.length);
+console.log(testStr.toUpperCase());
+console.log(testStr.charAt(5));
+console.log(testStr.indexOf('t'));
+console.log(testStr.substring(0,5));
+console.log(testStr.slice(-6, -1));
+console.log(testStr.trim());
+console.log(testStr.replaceAll('-','#'));
+console.log(testStr.includes("ore"));
+console.log(testStr.trim().split("-"));
+
+
+
+
 
